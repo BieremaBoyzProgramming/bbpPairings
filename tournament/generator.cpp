ab5037b066890a31ecbad820c944cb8b8db4e9d5
Sernin van de Krol <serninpc@paneidos.net>
2020-04-12 20:32:52 +0200

    Remove unexpected qualifier

--- tournament/generator.cpp
+++ tournament/generator.cpp
@@ -85,7 +85,7 @@ namespace tournament
     }
 
     template
-    MatchesConfiguration::MatchesConfiguration<std::minstd_rand>(
+    MatchesConfiguration::MatchesConfiguration(
       Configuration &&,
       std::minstd_rand &);
 
