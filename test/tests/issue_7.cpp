4b2c4ef12c1b208955e090a021880a64b095aebd
Jeremy Bierema <jbierema@zoho.com>
2022-01-09 16:50:17 -0500

    Revise test to make use of relative output path so it has test coverage

--- test/tests/issue_7.cpp
+++ test/tests/issue_7.cpp
@@ -1,13 +1,14 @@
 // https://github.com/BieremaBoyzProgramming/bbpPairings/issues/7
 void TEST_FUNCTION(const testing::Context &context)
 {
-  auto output_path = context.data_folder_path / STRINGIFY(TEST_ID) ".output";
+  auto output_filename = STRINGIFY(TEST_ID) ".output";
   testing::run(
     context.exe_path.string()
     + " --dutch "
     + (context.data_folder_path / STRINGIFY(TEST_ID) ".input").string()
     + " -p "
-    + output_path.string());
-  testing::assert_file_content_matches
-    (output_path, context.data_folder_path / STRINGIFY(TEST_ID) ".output.expected");
+    + output_filename);
+  testing::assert_file_content_matches(
+    context.data_folder_path / output_filename,
+    context.data_folder_path / STRINGIFY(TEST_ID) ".output.expected");
 }
