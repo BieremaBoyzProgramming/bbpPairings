3fbe55a1570ca746466be29baaa4574afa04564d
Marcel Juenemann <mail@marcel-juenemann.de>
2025-12-18 18:11:37 +0000

    Demo of passing strings back and forth

--- src/wasm.cpp
+++ src/wasm.cpp
@@ -6,6 +6,16 @@ float lerp(float a, float b, float t) {
     return (1 - t) * a + t * b;
 }
 
+std::string getGreeting() {
+    return "Hello from C++!";
+}
+
+std::string echoString(const std::string& input) {
+    return "Echo: " + input;
+}
+
 EMSCRIPTEN_BINDINGS(my_module) {
     function("lerp", &lerp);
+    function("getGreeting", &getGreeting);
+    function("echoString", &echoString);
 }
